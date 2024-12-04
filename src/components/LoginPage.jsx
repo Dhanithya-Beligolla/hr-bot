cc17f67fb41f2cdbd583d64d44832777da78125e
Dhanithya Beligolla <it22085580@my.sliit.lk>
2024-12-04 09:21:24 +0530

    ...

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -102,7 +102,7 @@ function LoginPage() {
                     href="#"
                     className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    Forgot password?
+                    Forgot password? ...
                   </a>
                 </div>
                 <button
