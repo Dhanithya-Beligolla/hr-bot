18f243cbe8ecb8a28c1313a037821dac99ba9a83
Dhanithya Beligolla <it22085580@my.sliit.lk>
2024-12-04 09:18:57 +0530

    ...

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -89,7 +89,7 @@ function LoginPage() {
                     htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Password
+                    Your Password
                   </label>
                   <InputWrapper>
                     <StyledInput
