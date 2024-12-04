4ff9cc948801f14490583f083779a9be2e27fd9c
Dhanithya Beligolla <it22085580@my.sliit.lk>
2024-12-04 09:13:29 +0530

    update login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -6,12 +6,36 @@ import { useNavigate } from 'react-router-dom';
 import './LoginPage.css'; // Add this for custom media queries
 import logo from '../assets/logo-dfccbank.png';
 import Footer from './Footer';
+import styled from 'styled-components';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledInput = styled.input`
+  padding-right: 30px;
+`;
+
+const ShowPasswordButton = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 5px;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
 
 function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -67,16 +91,21 @@ function LoginPage() {
                   >
                     Password
                   </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required=""
-                  />
+                  <InputWrapper>
+                    <StyledInput
+                      type={showPassword ? 'text' : 'password'}
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required=""
+                    />
+                    <ShowPasswordButton type="button" onClick={togglePasswordVisibility}>
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </ShowPasswordButton>
+                  </InputWrapper>
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
